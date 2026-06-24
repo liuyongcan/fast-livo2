c0e93f4d0891d3a9a36b2d181ec66e1f7cc08742
liuyongcan <954722597@qq.com>
2026-06-24 23:30:02 +0800

    提交自动对齐的代码

---
+++ auto_alignment/src/auto_align_node.cpp
@@ -0,0 +1,53 @@
+#include <fstream>
+#include <math.h>
+#include <vector>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <iostream>
+#include <string>
+#include <optional>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/search/impl/search.hpp>
+#include <pcl/range_image/range_image.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/common/common.h>
+#include <pcl/common/transforms.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/registration/icp.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/filter.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/octree/octree_pointcloud_voxelcentroid.h>
+#include <pcl/filters/crop_box.h> 
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
+
+#include <eigen3/Eigen/Dense>
+
+#include "aloam_velodyne/common.h"
+#include "aloam_velodyne/tic_toc.h"
+
+#include "scancontext/Scancontext.h"
+
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "auto_align_node");
+	ros::NodeHandle nh;
+
+ 	ros::spin();
+
+	return 0;
+}
